1080d766a7f3fa72c0a3eb2a1c662ca83189cf1d
SharkySK <jaku0584@stud.kea.dk>
2021-02-03 18:57:58 +0100

    smaller fix

--- OFF.py
+++ OFF.py
@@ -23,7 +23,7 @@ def color_off(strip, color, wait_ms=50):
 if __name__ == "__main__":
 
     # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHAN$
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     strip.begin()
 
