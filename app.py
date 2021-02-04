f0864232f2834c85e711c7936438ce8da26235a6
JakuPetrik <mirrorrsedge@gmail.com>
2021-02-04 16:30:07 +0100

    fix RGB

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def prog_file(prog_name):
     file = "{}/{}.py".format(UPLOAD_FOLDER, prog_name)
     print(file)
     try:
-        p = Popen(["python3", file, str(RGB[0]), str(RGB[1]), str(RGB[2])])
+        p = Popen(["python3", file, str(RGB[0]), str(RGB[2]), str(RGB[1])])
     except FileNotFoundError:
         print(FileNotFoundError, "error")
     except OSError:
